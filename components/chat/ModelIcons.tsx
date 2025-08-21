c67cda77771f0d1a385e9501ede7bbd0c91b7b3e
moneymindsetig2000 <moneymindset.ig2000@gmail.com>
2025-08-21 14:53:16 +0530

    fix: Update model icons for better representation
    Replaced SVG icons with text-based initials for GPT and DeepSeek models. Updated styling for DeepSeek to use a blue background.

--- components/chat/ModelIcons.tsx
+++ components/chat/ModelIcons.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const ChatGptIcon = () => (
     <div className="w-8 h-8 bg-[#10A37F] rounded-full flex items-center justify-center">
-        <svg className="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 24 24"><path d="M22.53,9.72,18,5.19,16.28,7,18,8.72,13.5,13.22,9,8.72l4.5-4.5L11.78,2.5,2.25,12l9.53,9.5,1.72-1.72-4.5-4.5,4.5-4.5,4.5,4.5L20.22,13Z"></path></svg>
+        <span className="text-white font-bold text-xs tracking-tighter">GPT</span>
     </div>
 );
 
@@ -13,18 +13,15 @@ export const GeminiIcon = () => (
 );
 
 export const DeepSeekIcon = () => (
-    <div className="w-8 h-8 flex items-center justify-center">
-      <svg width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M12,18.75L15.3,16.5L12,14.25L8.7,16.5L12,18.75M12,13.5L18.6,9.75L12,6L5.4,9.75L12,13.5M12,2L20,7L12,12L4,7L12,2Z"></path></svg>
+    <div className="w-8 h-8 bg-blue-500 rounded-full flex items-center justify-center">
+        <span className="text-white font-bold text-xs tracking-tighter">DS</span>
     </div>
 );
 
 export const PerplexityIcon = () => (
     <div className="w-8 h-8 flex items-center justify-center text-blue-500">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-            <path d="M10.5 13.5C10.5 15.1569 9.15685 16.5 7.5 16.5C5.84315 16.5 4.5 15.1569 4.5 13.5C4.5 11.8431 5.84315 10.5 7.5 10.5C9.15685 10.5 10.5 11.8431 10.5 13.5Z" />
-            <path d="M19.5 7.5C19.5 9.15685 18.1569 10.5 16.5 10.5C14.8431 10.5 13.5 9.15685 13.5 7.5C13.5 5.84315 14.8431 4.5 16.5 4.5C18.1569 4.5 19.5 5.84315 19.5 7.5Z" />
-            <path fillRule="evenodd" clipRule="evenodd" d="M3 13.5C3 10.4624 5.46243 8 8.5 8C11.5376 8 14 10.4624 14 13.5C14 16.5376 11.5376 19 8.5 19C5.46243 19 3 16.5376 3 13.5ZM8.5 9.5C6.29086 9.5 4.5 11.2909 4.5 13.5C4.5 15.7091 6.29086 17.5 8.5 17.5C10.7091 17.5 12.5 15.7091 12.5 13.5C12.5 11.2909 10.7091 9.5 8.5 9.5Z" />
-            <path fillRule="evenodd" clipRule="evenodd" d="M12 7.5C12 4.46243 14.4624 2 17.5 2C20.5376 2 23 4.46243 23 7.5C23 10.5376 20.5376 13 17.5 13C14.4624 13 12 10.5376 12 7.5ZM17.5 3.5C15.2909 3.5 13.5 5.29086 13.5 7.5C13.5 9.70914 15.2909 11.5 17.5 11.5C19.7091 11.5 21.5 9.70914 21.5 7.5C21.5 5.29086 19.7091 3.5 17.5 3.5Z" />
+            <path d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,4A8,8 0 0,1 20,12H18A6,6 0 0,0 12,6V4M4,12A8,8 0 0,1 12,4V6A6,6 0 0,0 6,12H4M12,20A8,8 0 0,1 4,12H6A6,6 0 0,0 12,18V20M20,12A8,8 0 0,1 12,20V18A6,6 0 0,0 18,12H20Z" />
         </svg>
     </div>
 );
